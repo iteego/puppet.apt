3a0dcfc9e8dae0934b08156f89f9d03dfc307d1f
Marcus Pemer <marcus@iteego.com>
2012-11-05 12:29:30 +0100

    Added daily apt-get update

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,26 @@
 class apt {
-  file {
-    "/etc/apt/sources.list":
-      content => template('apt/etc/apt/sources.list.erb'),
-      ensure => present;
-   }
 
-   exec { "/usr/bin/apt-get update":
-      alias => "aptgetupdate",
-      require => File["/etc/apt/sources.list"],
-      subscribe => File["/etc/apt/sources.list"],
-      refreshonly => true;
-   }
+  file { '/etc/apt/sources.list':
+    content => template('apt/etc/apt/sources.list.erb'),
+    ensure => present,
+  }
+
+  exec { '/usr/bin/apt-get update':
+    alias => "aptgetupdate",
+    require => File['/etc/apt/sources.list'],
+    subscribe => File['/etc/apt/sources.list'],
+    refreshonly => true,
+  }
+
+  schedule { 'daily':
+    period => daily,
+    range  => '2 - 4',
+  }
+
+  exec { '/usr/bin/apt-get update':
+    command => '/usr/bin/apt-get update',
+    require => File['/etc/apt/sources.list'],
+    schedule => 'daily',
+  }
 
 }
